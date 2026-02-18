b7184aaa212783af7cb047117592afc48f5c19ca
Technodon <subwayheimerwender@icloud.com>
2026-02-18 22:40:51 +0300

    index added

---
+++ calc.asm
@@ -0,0 +1,30 @@
+[org 800h]
+bits 16
+
+start:
+    mov ax, 0x03
+    int 0x10
+
+    mov si, input1_msg
+    call print_start
+
+    ;wait for keyboard input
+    mov ah, 0x00
+    int 0x16
+
+    jmp 500h
+
+print_start:
+    mov ah, 0x0e
+    mov bh, 0
+    mov bl, 0x04
+print_loop:
+    lodsb
+    cmp al, 0
+    je print_done
+    int 0x10
+    jmp print_loop
+print_done:
+    ret
+
+input1_msg: db 'Press A Key', 0
